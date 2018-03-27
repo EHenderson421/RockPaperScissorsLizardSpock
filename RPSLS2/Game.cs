3ee27b572425d62e8ab4db4d571e83247e914012
ehenderson421 <ehenderson421@gmail.com>
2018-03-27 15:21:44 -0500

    fix game class

--- RPSLS2/Game.cs
+++ RPSLS2/Game.cs
@@ -9,6 +9,7 @@ namespace RPSLS2
     class Game
     {
         //member variable
+
         int winningScore;
         public int playerOnePoint;
         public int playerTwoPoint;
@@ -17,25 +18,26 @@ namespace RPSLS2
 
 
         //constructor
+
         public Game()
         {
-            winningScore = 5;
+            
         }
 
         //membermethods
+    
             // choose your players
         public void ChoosePlayers()
         {
-            Console.WriteLine("Enter '1' for Single Player or Enter '2' for 2 Players");
-            string userInput = Console.ReadLine();
-            switch (userInput)
+            Console.WriteLine("Enter '1' for Single Player or Enter '2' for Two Players");
+            string numberOfPlayers = Console.ReadLine();
+            switch (numberOfPlayers)
             {
                 case "1":
                     playerOne = new Human();
                     playerTwo = new Computer();
                     playerOne.GetPlayerName();
                     playerOne.DisplayPlayerName();
-
                     Console.WriteLine("\n---------- " + playerOne.name + " ---------- VS ---------- " + playerTwo.name + " ----------");
                     break;
 
@@ -46,30 +48,48 @@ namespace RPSLS2
                     playerOne.DisplayPlayerName();
                     playerTwo.GetPlayerName();
                     playerTwo.DisplayPlayerName();
-
                     Console.WriteLine("\n---------- " + playerOne.name + " ---------- VS ---------- " + playerTwo.name + " ----------");
                     break;
 
                 default:
                     Console.WriteLine("\nPlease Try again");
-
                     ChoosePlayers();
                     break;
             }
         }
 
-            // players choose a move
-        public void ChooseYourPlay()
+            // length of game
+        public void LengthOfGame()
         {
-            // Console.Clear();
-            Console.WriteLine("\n" + playerOne.name + " Choose Your Play");
-            playerOne.ChooseOption();
-            //  Console.Clear();
-            Console.WriteLine("\n" + playerTwo.name + " Choose Your Play");
-            playerTwo.ChooseOption();
-            //  Console.Clear();
-        }
+            Console.WriteLine("Please Select Length Of Match");
+            Console.WriteLine("Enter '3' For Best Of 'THREE' Match");
+            Console.WriteLine("Enter '5' For Best Of 'FIVE' Match");
+            Console.WriteLine("Enter '7' For Best Of 'SEVEN' Match");
+            string matchLength = Console.ReadLine();
+            switch (matchLength)
+            {
+                case "3":
+                    winningScore = 2;
+                    Console.WriteLine("This Match Is Set For A Best 2 Out Of 3");
+                    break;
 
+                case "5":
+                    winningScore = 3;
+                    Console.WriteLine("This Match Is Set For A Best 3 Out Of 5");
+                    break;
+
+                case "7":
+                    winningScore = 4;
+                    Console.WriteLine("This Match Is Set For A Best 4 Out Of 7");
+                    break;
+
+                default:
+                    Console.WriteLine("\nPlease Try again");
+                    LengthOfGame();
+                    break;
+            }
+        }
+               
             // figure the winner of a throw and score a point
         public void ScoreWinningThrow()
         {
@@ -79,12 +99,12 @@ namespace RPSLS2
             }
             else if ((playerOne.possibleMove == "Rock" && playerTwo.possibleMove == "Scissors") || (playerOne.possibleMove == "Rock" && playerTwo.possibleMove == "Lizard") || (playerOne.possibleMove == "Paper" && playerTwo.possibleMove == "Rock") || (playerOne.possibleMove == "Paper" && playerTwo.possibleMove == "Spock") || (playerOne.possibleMove == "Scissors" && playerTwo.possibleMove == "Paper") || (playerOne.possibleMove == "Scissors" && playerTwo.possibleMove == "Lizard") || (playerOne.possibleMove == "Lizard" && playerTwo.possibleMove == "Spock") || (playerOne.possibleMove == "Lizard" && playerTwo.possibleMove == "Paper") || (playerOne.possibleMove == "Spock" && playerTwo.possibleMove == "Scissors") || (playerOne.possibleMove == "Spock" && playerTwo.possibleMove == "Rock"))
             {
-                Console.WriteLine("\n" + playerOne.name + " Won This Round and Scored 1 Point");
+                Console.WriteLine("\n" + playerOne.name + " Won This Round and Scored '1' Point");
                 playerOnePoint++;
             }
             else
             {
-                Console.WriteLine("\n" + playerTwo.name + " Won This Round and Scored 1 Point");
+                Console.WriteLine("\n" + playerTwo.name + " Won This Round and Scored '1' Point");
                 playerTwoPoint++;
             }
         }
@@ -100,11 +120,13 @@ namespace RPSLS2
         {
             if (playerOnePoint > 1)
             {
-                Console.WriteLine("\n----------" + playerOne.name + "----------" + " Wins The Match----------");
+                Console.WriteLine("\n----------" + playerOne.name + "----------");
+                Console.WriteLine("\n----------Wins The Match----------");
             }
             else if (playerTwoPoint > 1)
             {
-                Console.WriteLine("\n----------" + playerTwo.name + "----------" + " Wins The Match----------");
+                Console.WriteLine("\n----------" + playerTwo.name + "----------");
+                Console.WriteLine("\n----------Wins The Match----------");
             }
         }
 
@@ -113,9 +135,9 @@ namespace RPSLS2
 
         {
             Console.WriteLine("\n----------Would you like To Play Again?----------");
-            Console.WriteLine("\n---------Type '1' for Yes or '2' for no----------");
-            string playAgainYeasNo = Console.ReadLine();
-            switch (playAgainYeasNo)
+            Console.WriteLine("\n----------Type '1' for Yes or '2' for no----------");
+            string matchResetYesNo = Console.ReadLine();
+            switch (matchResetYesNo)
             {
                 case "1":
                     playerOnePoint = 0;
@@ -142,13 +164,20 @@ namespace RPSLS2
             do
             {
                 ChoosePlayers();
-
+                LengthOfGame();
                 while (playerOnePoint < winningScore && playerTwoPoint < winningScore)
                 {
+                    
+                    Console.WriteLine("\n" + playerOne.name + " Choose Your Play");
                     playerOne.ChooseOption();
+                    Console.Clear();
+                    Console.WriteLine("\n" + playerTwo.name + " Choose Your Play");
                     playerTwo.ChooseOption();
+                    
+
                     ScoreWinningThrow();
                     ScoreOfMatch();
+                    
                 }
                 FindWinnerOfMatch();
                 MatchReset();
